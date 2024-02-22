bbb862c83350cf9173efbb64785a5028977e6cfd
its-bismay <2023ugee070@nitjsr.ac.in>
2024-02-23 03:04:01 +0530

    files are updated

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
